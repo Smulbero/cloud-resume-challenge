b0c91cec20140f40dd0a0dabefe1ac7ea09f5a76
Smulbero <smulbero@gmail.com>
2026-07-17 20:45:23 +0300

    Frontend work, JS implementation started

--- frontend/js/scripts.js
+++ frontend/js/scripts.js
@@ -1,1 +1,42 @@
-const VISITOR_COUNTER_DISPLAY = document.getElementById("visitor-counter-display")
+async function initVisitorCounter() {
+    const visitorCounterContainerId = "visitor-counter-container";
+    const visitorCounterContainer = document.getElementById(visitorCounterContainerId);
+    // Exit early if container is not found
+    if (!visitorCounterContainer) {
+        console.error(`HTML element with id '${visitorCounterContainerId}' was not found`);
+        return;
+    }
+    
+    const apiEndpoint = ""
+    const visitorCounterElement = document.createElement("p");
+    const visitorCounterTextPrefix = "Site Visitor Counter:"
+    visitorCounterContainer.appendChild(visitorCounterElement);
+    
+    const data = await getData(apiEndpoint)
+    visitorCounterElement.textContent = data?.count ? 
+        `${visitorCounterTextPrefix} ${data.count}` :
+        `${visitorCounterTextPrefix} Error has occured`
+}
+
+async function getData(url){
+    if (typeof url != 'string') return
+
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`API responded with status code: ${response.status}`);
+        }
+
+        const data = await response.json()
+        return data
+    } catch (error) {
+        console.error(`Failed to fetch visitor count: ${error}`)
+        return
+    }
+}
+
+if (document.readyState === "loading") {
+    document.addEventListener("DOMContentLoaded", initVisitorCounter())
+} else {
+    initVisitorCounter()
+}
