b51a233f1d9b7a572b836f1894797f90d8e0f55a
smulbero <smulbero@gmail.com>
2026-07-26 10:49:26 +0300

    Storage Account added

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -1,5 +1,12 @@
 module "resource_groups" {
-  source = "./modules/resource-groups"
+  source          = "./modules/resource-groups"
   resource_groups = var.resource_groups
-  general_tags = var.general_tags
+  general_tags    = local.general_tags
+}
+
+module "storage_accounts" {
+  source           = "./modules/storage-accounts"
+  resource_groups  = module.resource_groups.resource_groups
+  storage_accounts = var.storage_accounts
+  general_tags     = local.general_tags
 }
--- infrastructure/terraform/variables.tf
+++ infrastructure/terraform/variables.tf
@@ -1,15 +1,47 @@
 variable "general_tags" {
   type        = map(string)
   description = "A mapping of global tags to assign to all resources"
-  default     = {
+  default = {
     terraform = true
   }
 }
 
+locals {
+  general_tags = merge(
+    { deployedDate = formatdate("DD-MM-YYYY", timestamp()) },
+    var.general_tags
+  )
+}
+
+# ==================================================
+# Resource Groups
+# ==================================================
 variable "resource_groups" {
   type = map(object({
-      location = string
-      tags = optional(map(string), {})
+    location = string
+    tags     = optional(map(string), {})
+  }))
+  description = "Map of resource group objects to deploy"
+}
+
+# ==================================================
+# Storage Accounts
+# ==================================================
+variable "storage_accounts" {
+  type = map(object({
+    # Required attributes
+    name                     = string
+    account_tier             = string
+    account_replication_type = string
+
+    # Optional attributes
+    account_kind = optional(string, "Storage")
+    access_tier  = optional(string, "Hot")
+    static_website = optional(object({
+      index_document     = optional(string, "index.html")
+      error_404_document = optional(string, "404.html")
     }))
-    description = "Map of resource group objects to deploy"
+    tags = optional(map(string), {})
+  }))
+  description = "Map of storage account objects to deploy"
 }
