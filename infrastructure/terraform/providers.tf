8cf7b22d2e2e423bcba235edfd063c7e22a45cec
smulbero <smulbero@gmail.com>
2026-07-26 10:48:45 +0300

    Random provider added

--- infrastructure/terraform/providers.tf
+++ infrastructure/terraform/providers.tf
@@ -2,13 +2,17 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=4.1.0"
+      version = "~> 4.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~>3.0"
     }
   }
 }
 
 provider "azurerm" {
   features {
-    
+
   }
 }
