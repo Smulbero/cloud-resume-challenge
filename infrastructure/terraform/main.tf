fd9f765442e34480727d7d4f96a5a4cd304cb411
Smulbero <smulbero@gmail.com>
2026-07-29 21:25:15 +0300

    Service plans and function apps implemented

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -1,12 +1,41 @@
+# ==================================================
+# Resource Groups
+# ==================================================
 module "resource_groups" {
   source          = "./modules/resource-groups"
   resource_groups = var.resource_groups
   general_tags    = local.general_tags
 }
 
+# ==================================================
+# Storage Accounts
+# ==================================================
 module "storage_accounts" {
   source           = "./modules/storage-accounts"
   resource_groups  = module.resource_groups.resource_groups
   storage_accounts = var.storage_accounts
   general_tags     = local.general_tags
+}
+
+# ==================================================
+# Service Plans
+# ==================================================
+module "service_plans" {
+  source          = "./modules/service-plans"
+  resource_groups = module.resource_groups.resource_groups
+  service_plans   = var.service_plans
+  general_tags    = local.general_tags
+}
+
+# ==================================================
+# Function Apps
+# ==================================================
+module "function_apps" {
+  source             = "./modules/function-apps"
+  resource_groups    = module.resource_groups.resource_groups
+  storage_accounts   = module.storage_accounts.storage_accounts
+  storage_containers = module.storage_accounts.storage_containers
+  service_plans      = module.service_plans.service_plans
+  function_apps      = var.function_apps
+  general_tags       = local.general_tags
 }
--- infrastructure/terraform/variables.tf
+++ infrastructure/terraform/variables.tf
@@ -43,6 +43,50 @@ variable "storage_accounts" {
       error_404_document = optional(string, "404.html")
     }))
     tags = optional(map(string), {})
+
+    container = optional(object({
+      name                  = optional(string, null)
+      container_access_type = optional(string, null)
+    }), null)
   }))
   description = "Map of storage account objects to deploy"
+}
+
+# ==================================================
+# Service Plans
+# ==================================================
+variable "service_plans" {
+  type = map(object({
+    # Required attributes
+    resource_group_key = string
+    name               = string
+    os_type            = string
+    sku_name           = string
+
+    # Optional attributes
+    tags = optional(map(string), {})
+  }))
+  description = "Map of service plan objects to deploy"
+}
+
+# ==================================================
+# Function Apps
+# ==================================================
+variable "function_apps" {
+  type = map(object({
+    # Required attributes
+    resource_group_key          = string
+    storage_account_key         = string
+    service_plan_key            = string
+    name                        = string
+    storage_container_type      = optional(string, "blobContainer")
+    storage_authentication_type = optional(string, "SystemAssignedIdentity")
+    runtime_name                = string
+    runtime_version             = number
+
+    # Optional attributes
+    app_settings = optional(map(string), {})
+    tags = optional(map(string), {})
+  }))
+  description = "Map of function app objects to deploy"
 }
