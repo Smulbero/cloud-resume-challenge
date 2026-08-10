9d9b1417dff8995a3db3f734a44265ce35cb52f1
Smulbero <smulbero@gmail.com>
2026-08-10 19:27:44 +0300

    outputs updated

--- infrastructure/terraform/outputs.tf
+++ infrastructure/terraform/outputs.tf
@@ -1,13 +1,30 @@
-output "resource_group_names" {
-  value = module.resource_groups.resource_group_names
+output "resource_groups" {
+  value = {
+    for k, v in module.resource_groups.resource_groups : k => {
+        name = v.name
+      }
+    }
 }
 
-output "cosmos_db_names" {
-  value = module.cosmos_dbs.cosmos_db_names
+output "cosmos_db_accounts" {
+  value = {
+    for k, v in module.cosmos_dbs.cosmos_db_accounts : k => {
+      name = v.name
+    }
+  }
 }
 
-output "function_app_identities" {
-  value = module.function_apps.function_app_identities
+# output "function_app_identities" {
+#   value = module.function_apps.function_app_identities
+# }
+
+output "function_apps_flex" {
+  value = {
+    for k, v in module.function_apps.function_apps_flex : k => {
+      name = v.name
+      principal_id = v.identity[*].principal_id
+    } 
+  }
 }
 
 output "frontdoor_custom_domain_validation_tokens" {
