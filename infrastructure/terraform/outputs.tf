6a8ced3261fac05706f473b79d3175f9ced909ca
Smulbero <smulbero@gmail.com>
2026-08-10 18:51:33 +0300

    outputs added

--- infrastructure/terraform/outputs.tf
+++ infrastructure/terraform/outputs.tf
@@ -1,3 +1,15 @@
+output "resource_group_names" {
+  value = module.resource_groups.resource_group_names
+}
+
+output "cosmos_db_names" {
+  value = module.cosmos_dbs.cosmos_db_names
+}
+
+output "function_app_identities" {
+  value = module.function_apps.function_app_identities
+}
+
 output "frontdoor_custom_domain_validation_tokens" {
   value = module.frontdoors.frontdoor_custom_domain_validation_tokens
 }
